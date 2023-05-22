01ac7d62d7221451eb37af24e51f3711e6d26ba3
ViniciusGabrielRomie <130675071+ViniciusGabrielRomie@users.noreply.github.com>
2023-05-22 20:34:22 -0300

    Add files via upload

---
+++ tela_Favorito.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({reset: true });
+
+sr.reveal('.fav1' , {duration: 2000});
+sr.reveal('.fav2' , {duration: 2000});
+sr.reveal('.fav3' , {duration: 2000});
+sr.reveal('.fav4' , {duration: 2000});
+sr.reveal('.fav5' , {duration: 2000});
+sr.reveal('.fav6' , {duration: 2000});
+sr.reveal('.fav7' , {duration: 2000});
+sr.reveal('.fav8' , {duration: 2000});
+sr.reveal('.fav9' , {duration: 2000});
