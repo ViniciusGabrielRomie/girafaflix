d7801615a555a3465c140ebc571ec5ddb0c9999c
ViniciusGabrielRomie <130675071+ViniciusGabrielRomie@users.noreply.github.com>
2023-05-30 20:41:29 -0300

    Update tela_Favorito.js

--- tela_Favorito.js
+++ tela_Favorito.js
@@ -9,3 +9,62 @@ sr.reveal('.fav6' , {duration: 2000});
 sr.reveal('.fav7' , {duration: 2000});
 sr.reveal('.fav8' , {duration: 2000});
 sr.reveal('.fav9' , {duration: 2000});
+
+
+const video = document.querySelector('.fav1');
+const video1 = document.querySelector('.fav2');
+const video2 = document.querySelector('.fav3');
+const video3 = document.querySelector('.fav4');
+const video4 = document.querySelector('.fav5');
+const video5 = document.querySelector('.fav6');
+const video6 = document.querySelector('.fav7');
+const video7 = document.querySelector('.fav8');
+const video8 = document.querySelector('.fav9');
+
+video.addEventListener('click', () => {
+    window.open("https://www.youtube.com/watch?v=mdIfxd7hII4","_blank");
+})
+
+video1.addEventListener('click', () => {
+    window.open("https://www.youtube.com/watch?v=mdIfxd7hII4","_blank");
+})
+
+video2.addEventListener('click', () => {
+    window.open("https://www.youtube.com/watch?v=mdIfxd7hII4","_blank");
+})
+
+video3.addEventListener('click', () => {
+    window.open("https://www.youtube.com/watch?v=mdIfxd7hII4","_blank");
+})
+
+video4.addEventListener('click', () => {
+    window.open("https://www.youtube.com/watch?v=mdIfxd7hII4","_blank");
+})
+
+video5.addEventListener('click', () => {
+    window.open("https://www.youtube.com/watch?v=mdIfxd7hII4","_blank");
+})
+
+video6.addEventListener('click', () => {
+    window.open("https://www.youtube.com/watch?v=mdIfxd7hII4","_blank");
+})
+
+video7.addEventListener('click', () => {
+    window.open("https://www.youtube.com/watch?v=mdIfxd7hII4","_blank");
+})
+
+video8.addEventListener('click', () => {
+    window.open("https://www.youtube.com/watch?v=mdIfxd7hII4","_blank");
+})
+
+
+
+$('.fav1').css('cursor','pointer');
+$('.fav2').css('cursor','pointer');
+$('.fav3').css('cursor','pointer');
+$('.fav4').css('cursor','pointer');
+$('.fav5').css('cursor','pointer');
+$('.fav6').css('cursor','pointer');
+$('.fav7').css('cursor','pointer');
+$('.fav8').css('cursor','pointer');
+$('.fav9').css('cursor','pointer');
